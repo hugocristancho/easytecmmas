6afdb318679b160a50e36a786e5e65f2a0be1f72
hugocristancho <43221629+hugocristancho@users.noreply.github.com>
2018-09-12 16:39:17 -0500

    Primera version
    hugo
    
    Co-Authored-By: Hugo Jobling <hello@thisishugo.com>

---
+++ application/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$autoload['packages'] = array();
+
+
+$autoload['libraries'] = array('database');
+
+
+$autoload['drivers'] = array();
+
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
+
+$autoload['model'] = array();
